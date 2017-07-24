af96e75e1d5a8bc595896fe81bf639ad04ca934c
liang <passager@163.com>
2017-07-24 08:00:46 +0800

    add redis

---
+++ YFZX/redis_test.py
@@ -0,0 +1,21 @@
+import redis
+
+
+
+pool1=redis.ConnectionPool(host='localhost', port=6379)
+redis1=redis.Redis(connection_pool=pool1)
+
+
+
+# for ll in range(5,15):
+#     print redis1.hset('llde',ll,2)
+# print redis1.hlen('llde')
+# print redis1.hget('llde',5)
+#
+# print redis1.lpush('one',1)
+# print redis1.rpop('one')
+# print redis1.lpush('one','one')
+# print redis1.lrange('one',0,100)
+
+
+print redis1.hset('llde','yixie',1)
